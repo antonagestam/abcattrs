621dbd87b5e0eaf3d1afd4b5e970fd64af76637f
Anton Agestam <git@antonagestam.se>
2022-09-06 16:51:13 +0200

    almost progress ...

--- abcattrs_plugin.py
+++ abcattrs_plugin.py
@@ -1,65 +1,116 @@
+from __future__ import annotations
+
 from typing import Callable
-from typing import Optional
+from typing import Final
+from typing import TypeAlias
 from typing import TypeVar
 
+from mypy.nodes import MDEF
+from mypy.nodes import Block
+from mypy.nodes import ClassDef
+from mypy.nodes import Context
+from mypy.nodes import SymbolTable
 from mypy.nodes import TypeInfo
+from mypy.nodes import Var
+from mypy.plugin import AnalyzeTypeContext
 from mypy.plugin import ClassDefContext
 from mypy.plugin import Plugin
+from mypy.types import Instance
 
-T = TypeVar("T")
-CB = Optional[Callable[[T], None]]
-
-abc_fullname = "abc.ABC"
+abc_fullname: Final = "abc.ABC"
+abstract_fullname: Final = "abcattrs.abcattrs.Abstract"
 
 
 def is_abstract(info: TypeInfo) -> bool:
-    # info.mro vs info.bases
+    return any(base.type.fullname == abc_fullname for base in info.bases)
+
+
+def has_abstract_ancestor(info: TypeInfo) -> bool:
     return any(base.fullname == abc_fullname for base in info.mro)
 
 
-def has_abstract_ancestor():
-    ...
-
-
-def is_concrete():
-    ...
-
-
 def mark_abstract_class_attrs(context: ClassDefContext) -> None:
-    if context.cls.fullname != "prog.B":
-        # print(context.cls.base_type_exprs[0])
-        # print(f"{context.cls.metaclass=}")
+    if is_abstract(context.cls.info):
         return
+    if not has_abstract_ancestor(context.cls.info):
+        return
+
+    print(f"checking {context.cls.fullname}")
 
     # for base in context.cls.info.bases:
     #     print(base)
-    abstract_attrs = set()
+    assignments = set()
+    abstract_definitions = set()
     for base in context.cls.info.mro:
         for name, defn in base.names.items():
-            # print(name, )
-            print(defn.kind)
-            print(defn.)
-            # print(defn.node.fullname, defn.type)
+            # Filter for class member definitions only.
+            if defn.kind != MDEF or defn.node is None or not isinstance(defn.node, Var):
+                continue
+            if defn.node.has_explicit_value:
+                assignments.add(defn.node.name)
+                print(f"{base.fullname} assigns {defn.node.name}")
 
-    print(context.cls.info.names)
+            if defn.node.type is None:
+                continue
 
-    # print(context.cls.base_type_exprs)
-    # print(context.cls.keywords)
-    # print(context.cls.info)
-    # print(context.cls.info.metadata)
-    # print(context.cls.info.names)
-    # print(context.cls.info.abstract_attributes)
-    # print(context.cls.info.is_abstract)
+            if not isinstance(defn.node.type, Instance):
+                continue
+
+            namespace = defn.node.type.type.metadata.get("abcattrs", {})
+            if namespace.get("is_abstract", False):
+                print(f"{base.fullname} defines {defn.node.name}")
+                abstract_definitions.add(defn.node.name)
+
+    if missing_assignments := abstract_definitions - assignments:
+        failing_context = Context()
+        failing_context.set_line(
+            target=context.cls.line,
+            column=context.cls.column,
+            end_line=context.cls.end_line,
+            end_column=context.cls.end_column,
+        )
+        for missing_name in missing_assignments:
+            context.api.fail(
+                (
+                    f"Concrete class {context.cls.name!r} is missing a value for "
+                    f"abstract attribute {missing_name!r}."
+                ),
+                failing_context,
+            )
+
+
+# def create_type_info(name: str, bases: list[Instance]) -> TypeInfo:
+#     classdef = ClassDef(name, Block([]))
+#     classdef.fullname = "abcattrs.sentinel"
+#     new_typeinfo = TypeInfo(SymbolTable(), classdef, "abcattrs")
+#     new_typeinfo.bases = bases
+#     new_typeinfo.metadata = {"abcattrs": "was here!"}
+#     # calculate_mro(new_typeinfo)
+#     # new_typeinfo.calculate_metaclass_type()
+#     classdef.info = new_typeinfo
+#     return new_typeinfo
+
+
+def type_analyze(context: AnalyzeTypeContext) -> None:
+    inner_type = context.api.anal_type(context.type.args[0])
+    assert isinstance(inner_type, Instance)
+    # FIXME: Need to create a unique typeinfo here?
+    inner_type.type.metadata.setdefault("abcattrs", {"is_abstract": True})
+    return inner_type
+
+
+T = TypeVar("T")
+CB: TypeAlias = Callable[[T], None] | None
 
 
 class ABCAttrsPlugin(Plugin):
-    def get_base_class_hook(self, fullname: str) -> "CB[ClassDefContext]":
-        sym = self.lookup_fully_qualified(fullname)
+    def get_type_analyze_hook(self, fullname: str) -> CB[AnalyzeTypeContext]:
+        if fullname == abstract_fullname:
+            return type_analyze
 
-        if sym and isinstance(sym.node, TypeInfo) and is_abstract(sym.node):
-            return mark_abstract_class_attrs
-        return None
+    def get_base_class_hook(self, fullname: str) -> CB[ClassDefContext]:
+        return mark_abstract_class_attrs
 
 
-def plugin(_version: str):
+def plugin(_version: str) -> type[ABCAttrsPlugin]:
     return ABCAttrsPlugin
--- prog.py
+++ prog.py
@@ -2,12 +2,37 @@ import abc
 
 from abcattrs import Abstract
 
+# from typing import _GenericAlias
+# from typing import _SpecialForm
+# from typing import _type_check
+
+
+# @_SpecialForm
+# def Abstract(self, parameters):
+#     item = _type_check(parameters, f"{self} accepts only single type.")
+#     return _GenericAlias(self, (item,))
+
 
 class A(abc.ABC):
     foo: Abstract[int]
+    bar: Abstract[int]
+    other: int
 
 
-# Should error
-class B(A):
+class B(A, abc.ABC):
+    baz: Abstract[int]
+    bar = 1
+
+
+# Should error: missing foo, missing baz
+class C(B):
     irrelevant = True
     undefined: bool
+
+
+class Foo:
+    ...
+
+
+class Bar(Foo):
+    ...
