42e1d1b908348abcb48f5527f8aa3c2e0c40618e
Dawn McKnight <demcknig@ualberta.ca>
2024-04-09 19:01:34 -0600

    Applying pyupgrade and isort to abcattrs/typing_redirect.py

--- abcattrs/typing_redirect.py
+++ abcattrs/typing_redirect.py
@@ -1,34 +1,25 @@
 import sys
+from typing import Callable
+from typing import ClassVar
+from typing import Dict
+from typing import Final
+from typing import ForwardRef
+from typing import Iterable
+from typing import Tuple
+from typing import Type
+from typing import Union
 
 if sys.version_info < (3, 9):
     from typing_extensions import Annotated
     from typing_extensions import Any
-    from typing_extensions import Callable
-    from typing_extensions import ClassVar
-    from typing_extensions import Dict
-    from typing_extensions import Final
-    from typing_extensions import ForwardRef
-    from typing_extensions import Iterable
-    from typing_extensions import Tuple
-    from typing_extensions import Type
     from typing_extensions import TypeVar
-    from typing_extensions import Union
     from typing_extensions import get_args
     from typing_extensions import get_origin
     from typing_extensions import get_type_hints
 else:
     from typing import Annotated  # type: ignore[attr-defined]
     from typing import Any
-    from typing import Callable
-    from typing import ClassVar
-    from typing import Dict
-    from typing import Final
-    from typing import ForwardRef
-    from typing import Iterable
-    from typing import Tuple
-    from typing import Type
     from typing import TypeVar
-    from typing import Union
     from typing import get_args
     from typing import get_origin
     from typing import get_type_hints
